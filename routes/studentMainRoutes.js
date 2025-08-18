e4eb53ea58d3c313c3a45c40f4c00f8d5559ecbf
dev03lkomavyax <dev03lko@mavyax.com>
2025-08-18 19:16:18 +0530

    code updated

--- routes/studentMainRoutes.js
+++ routes/studentMainRoutes.js
@@ -8,12 +8,12 @@ const { protect } = require("../middlewares/protectRoutes");
 const upload = require("../middlewares/imgUpload");
 const router = express.Router();
 
-router.patch("/change-password", protect, changePassword);
+router.patch("/change-password", changePassword);
 
 // TODO:
 // 1. forget password
 
-router.patch("/update-profile", protect, upload.single("image"), updateProfile);
-router.get("/get-profile", protect, getProfile);
+router.patch("/update-profile", upload.single("image"), updateProfile);
+router.get("/get-profile", getProfile);
 
 module.exports = router;

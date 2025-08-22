5b3b8abff32f571cfd3e0411706165b19aab70e5
dev03lkomavyax <dev03lko@mavyax.com>
2025-08-22 10:20:12 +0530

    code updated

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -18,7 +18,7 @@ const router = express.Router();
 router.post(
   "/create",
   // protect,
-  authorize("admin", "read&write"),
+  // authorize("admin", "read&write"),
   upload.single("image"),
   createAdmin
 );
@@ -27,39 +27,39 @@ router.post("/login", loginAdmin);
 router.get(
   "/get",
   // protect,
-  authorize("admin", "read"),
+  // authorize("admin", "read"),
   getAdmins
 );
 router.get(
   "/get-details/:id",
   // protect,
-  authorize("admin", "read"),
+  // authorize("admin", "read"),
   getAdminDetails
 );
 router.patch(
   "/update/:id",
   // protect,
-  authorize("admin", "read&write"),
+  // authorize("admin", "read&write"),
   upload.single("image"),
   updateAdmin
 );
 router.patch(
   "/update-status/:id",
   // protect,
-  authorize("admin", "read&write"),
+  // authorize("admin", "read&write"),
   updateAdminStatus
 );
 router.delete(
   "/delete/:id",
   // protect,
-  authorize("admin", "read&write"),
+  // authorize("admin", "read&write"),
   deleteAdmin
 );
 
 router.patch(
   "/change-password",
   // protect,
-  authorize("admin", "read&write"),
+  // authorize("admin", "read&write"),
   changePassword
 );
 router.post(
